eb10e187f2510c3dd7c6e619485139c0a379f4b0
Wan <wanzelin007@gmail.com>
2018-01-13 15:46:01 -0700

    getBuidingName.py

--- map/getBuildingName.py
+++ map/getBuildingName.py
@@ -1,2 +1,21 @@
-URL = input("give me URL: ")
-print(URL)
+import urllib
+import re
+
+URL = "http://map.arizona.edu/json/buildings.json"
+content = urllib.urlopen(URL)
+word = content.read()
+
+#word = '"Name": "Al-Marah - Caretaker Residence","AliasName": " "'
+
+building_list= re.findall(r'"Name":"[^"]+',word)
+
+my_dict = {}
+for x in range(0,len(building_list)):
+    my_dict[building_list[x][8:]] = 0;
+
+for value in my_dict.iteritems():
+    print value
+
+
+
+#print word_list
